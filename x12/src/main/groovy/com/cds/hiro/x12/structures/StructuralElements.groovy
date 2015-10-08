a0252b3af1e6fb0f016448b23d024cf180e35cfd
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-10-08 16:07:22 -0700

    Segments with strings and enums can be parsed

--- x12/src/main/groovy/com/cds/hiro/x12/structures/StructuralElements.groovy
+++ x12/src/main/groovy/com/cds/hiro/x12/structures/StructuralElements.groovy
@@ -1,6 +1,9 @@
 package com.cds.hiro.x12.structures
 
 abstract class Element {}
+abstract class Composite extends Element {
+
+}
 abstract class BlockElement extends Element {}
 
 abstract class Message extends BlockElement {
@@ -11,4 +14,24 @@ abstract class Loop extends BlockElement {
 }
 abstract class Segment extends BlockElement {
   abstract void parse(List<List<String[]>> input)
+
+  protected static <T> T valueOf(List<String[]> strings, Class<T> clazz) {
+    if (Enum.isAssignableFrom(clazz)) {
+      clazz.byCode(strings[0][0])
+    } else if (clazz == String) {
+      strings[0][0]
+    } else {
+      throw new Exception("Unhandled type")
+    }
+  }
+
+  protected static <T> List<T> listOf(List<String[]> strings, Class<T> clazz) {
+    if (Enum.isAssignableFrom(clazz)) {
+      strings.collect { clazz.byCode(it[0]) }
+    } else if (clazz == String) {
+      strings.collect {it[0]}
+    } else {
+      throw new Exception("Unhandled type")
+    }
+  }
 }
--- x12/src/test/groovy/EdiParserSpec.groovy
+++ x12/src/test/groovy/EdiParserSpec.groovy
@@ -1,4 +1,6 @@
 import com.cds.hiro.x12.EdiParser
+import com.cds.hiro.x12_837p.enums.TransactionSetIdentifierCode
+import com.cds.hiro.x12_837p.segments.ST
 import spock.lang.Specification
 
 /**
@@ -20,18 +22,19 @@ ST*Foo Bar*Fubar
         [
             [['ISA']],
             [['12']],
-            [['23']]
+            [['23']],
         ],
         [
             [['ST']],
             [['Foo Bar']],
-            [['Fubar']]
+            [['Fubar']],
         ]
     ]
   }
+
   def "Tokenizing can be customized"() {
     given: "A builder"
-    def parser = new EdiParser().setSegmentSeperator('~').setFieldSeperator(/\|/)
+    def parser = new EdiParser().segmentSeperator('~').fieldSeperator(/\|/)
 
     when: "I give it inputs without composites or reps"
     def tree = parser.extractTree("""ISA|12|23~ST|Foo Bar|Fubar""")
@@ -41,13 +44,52 @@ ST*Foo Bar*Fubar
         [
             [['ISA']],
             [['12']],
-            [['23']]
+            [['23']],
         ],
         [
             [['ST']],
             [['Foo Bar']],
-            [['Fubar']]
+            [['Fubar']],
         ]
     ]
+  }
+
+  def "A segment can be parsed"() {
+    given: "A segment tree"
+    def segmentTree =
+        [
+            [['ST']],
+            [['837']],
+            [['Fubar']],
+            [['Snafu']],
+        ]
+
+    when: "I parse it into an ST"
+    ST segment = new ST()
+    segment.parse(segmentTree)
+
+    then: "Segment is valid"
+    segment.transactionSetIdentifierCode_01 == TransactionSetIdentifierCode.HealthCareClaim_837
+    segment.transactionSetControlNumber_02 == 'Fubar'
+    segment.implementationConventionReference_03 == 'Snafu'
+  }
+
+  def "A segment can be parsed even if it is incomplete"() {
+    given: "A segment tree"
+    def segmentTree =
+        [
+            [['ST']],
+            [['837']],
+            [['Fubar']],
+        ]
+
+    when: "I parse it into an ST"
+    ST segment = new ST()
+    segment.parse(segmentTree)
+
+    then: "Segment is valid"
+    segment.transactionSetIdentifierCode_01 == TransactionSetIdentifierCode.HealthCareClaim_837
+    segment.transactionSetControlNumber_02 == 'Fubar'
+    segment.implementationConventionReference_03 == null
   }
 }
